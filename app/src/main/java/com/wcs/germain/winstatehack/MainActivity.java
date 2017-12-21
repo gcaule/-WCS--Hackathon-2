f2ae7b112896d6c457f65792f43dbe6fd21fdb11
Germain Caule <g.caule31@outlook.fr>
2017-12-21 14:51:51 +0100

    Added connection with password and user creation

--- app/src/main/java/com/wcs/germain/winstatehack/MainActivity.java
+++ app/src/main/java/com/wcs/germain/winstatehack/MainActivity.java
@@ -2,12 +2,15 @@ package com.wcs.germain.winstatehack;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
     }
 }
---
+++ app/src/main/java/com/wcs/germain/winstatehack/User.java
@@ -0,0 +1,49 @@
+package com.wcs.germain.winstatehack;
+
+/**
+ * Created by apprenti on 21/12/17.
+ */
+
+public class User {
+
+    String firstName;
+    String mail;
+    String password;
+
+    public User() {
+    }
+
+    public User(String name, String mail, String password) {
+        this.firstName = name;
+        this.mail = mail;
+        this.password = password;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+
+    public String getMail() {
+        return mail;
+    }
+
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
+
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+}
