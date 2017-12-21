8cf3c291d59cf7eedaefdd5cfa67fba88d9af8e1
Germain Caule <g.caule31@outlook.fr>
2017-12-22 00:05:40 +0100

    Added FACE API

--- app/src/main/java/com/wcs/germain/winstatehack/MenuCards.java
+++ app/src/main/java/com/wcs/germain/winstatehack/MenuCards.java
@@ -1,14 +1,11 @@
-package com.wcs.germain.winstatehack.Cards;
+package com.wcs.germain.winstatehack;
 
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.widget.TextView;
-
-import com.wcs.germain.winstatehack.R;
 
 public class MenuCards extends AppCompatActivity {
 
