48a321dbaf255632456f2cf5b763c6b5c050a2ea
Germain Caule <g.caule31@outlook.fr>
2017-12-21 15:13:54 +0100

    Worked

---
+++ app/src/main/java/com/wcs/germain/winstatehack/ContactActivity.java
@@ -0,0 +1,71 @@
+package com.wcs.germain.winstatehack;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+
+public class ContactActivity extends AppCompatActivity {
+
+    private ListView contactListView;
+    private DatabaseReference mContactDatabaseReference;
+    private ArrayList<User> contactList = new ArrayList<>();
+    private FirebaseDatabase mDatabase;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+        contactListView = findViewById(R.id.listview_contact);
+        TextView contacts = findViewById(R.id.contact_tv);
+        TextView addContact = findViewById(R.id.contact_new);
+
+        // TODO remove comments when assets is ready
+        // Assign font
+//        Typeface regularFont = Typeface.createFromAsset(getAssets(), "fonts/Montserrat_Regular.otf");
+//        Typeface boldFont = Typeface.createFromAsset(getAssets(), "fonts/Montserrat_Bold.otf");
+//        contacts.setTypeface(boldFont);
+//        addContact.setTypeface(boldFont);
+
+        // on charge les référent et on applique l'adapter
+        initFirebase();
+        addContactFirebaseListener();
+    }
+
+    private void initFirebase() {
+        mDatabase = FirebaseDatabase.getInstance();
+        mContactDatabaseReference = mDatabase.getReference();
+    }
+
+    private void addContactFirebaseListener() {
+        mContactDatabaseReference.child("user").orderByChild("firstName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                if (contactList.size() > 0)
+                    contactList.clear();
+                for(DataSnapshot postSnapshot:dataSnapshot.getChildren()){
+                    User contact = postSnapshot.getValue(User.class);
+                    contactList.add(contact);
+                }
+                ContactAdapter adapter = new ContactAdapter(ContactActivity.this, contactList);
+                contactListView.setAdapter(adapter);
+                contactListView.setVisibility(View.VISIBLE);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/wcs/germain/winstatehack/ContactAdapter.java
@@ -0,0 +1,64 @@
+package com.wcs.germain.winstatehack;
+
+import android.app.Activity;
+import android.content.Context;
+import android.graphics.Typeface;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.List;
+
+/**
+ * Created by wilder on 21/12/17.
+ */
+
+public class ContactAdapter  extends BaseAdapter{
+
+    private Activity mActivity;
+    private List<User> mListeContact;
+    private LayoutInflater mInflater;
+
+    public ContactAdapter(Activity activity, List<User> listeContact) {
+        this.mActivity = activity;
+        this.mListeContact = listeContact;
+    }
+
+    @Override
+    public int getCount() {
+        return mListeContact.size();
+    }
+
+    @Override
+    public Object getItem(int i) {
+        return mListeContact.get(i);
+    }
+
+    @Override
+    public long getItemId(int i) {
+        return i;
+    }
+
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        mInflater = (LayoutInflater) mActivity.getBaseContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View itemView = mInflater.inflate(R.layout.contacts_item,null);
+
+        TextView nomContact = itemView.findViewById(R.id.nom_contact);
+        String nom = mListeContact.get(i).getFirstName() ;
+        nomContact.setText(nom);
+
+        // TODO remove comments when assets is ready
+//        Typeface regularFont = Typeface.createFromAsset(mActivity.getAssets(), "fonts/Montserrat_Regular.otf");
+//        nomContact.setTypeface(regularFont);
+
+        return itemView;
+    }
+}
+
+
+
+
+
